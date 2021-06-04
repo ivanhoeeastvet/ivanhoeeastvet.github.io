37ea935ef0235f536e119854488dcd6a98b7d860
angeal185 <beneaves01@hotmail.com>
2021-06-04 18:16:34 +1000

    update

--- app/modules/main.mjs
+++ app/modules/main.mjs
@@ -2,6 +2,20 @@
 const app = {
     init(){
       console.log('hello world!')
+    },
+    messenger(){
+
+      window.Tawk_API = Tawk_API || {},
+      Tawk_LoadStart=new Date();
+
+      var s1=document.createElement("script");
+
+      s1.async=true;
+      s1.src='https://embed.tawk.to/60b9df3c6699c7280daaa98f/1f7b1024k';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      document.body.append(s1)
+
     }
 }
 
